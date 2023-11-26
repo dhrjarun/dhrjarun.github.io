3a654ca1f0a40e92605b05ac98b3b9982fe49050
Dhiraj Arun <dhrjarun@gmail.com>
2023-11-26 16:59:17 +0530

    add rehype-external-links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,29 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-
-import mdx from "@astrojs/mdx";
+import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
+import remarkParse from 'remark-parse';
+import rehypeStringify from 'rehype-stringify';
+import rehypeExternalLinks from 'rehype-external-links';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
   site: 'https://dhrjarun.github.io',
-  base: '/'
+  base: '/',
+  markdown: {
+    rehypePlugins: [
+      remarkParse,
+      rehypeStringify,
+      [
+        rehypeExternalLinks,
+        {
+          target: '_blank',
+          content: {
+            type: 'text',
+            value: '↗',
+          },
+        },
+      ],
+    ],
+  },
 });
