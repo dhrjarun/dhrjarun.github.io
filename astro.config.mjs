8c7f1bdb42d8f7315466a271fd620534713aa4f2
Dhiraj Arun <dhrjarun@gmail.com>
2023-12-27 10:14:34 +0530

    add vercel, change domain, upgrade astro to v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,21 @@ import remarkParse from 'remark-parse';
 import rehypeStringify from 'rehype-stringify';
 import { remarkReadingTime } from './remark-reading-time.mjs';
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
-  site: 'https://dhrjarun.github.io',
+  site: 'https://dhrjarun.com',
   base: '/',
   markdown: {
     remarkPlugins: [remarkReadingTime],
-    rehypePlugins: [remarkParse, rehypeStringify],
+    rehypePlugins: [remarkParse, rehypeStringify]
   },
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  })
 });
