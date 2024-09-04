7b63018b45e99f01fe97d8d22a4dd6d0ad182594
Dhiraj Arun <dhrjarun@gmail.com>
2024-09-04 16:54:27 +0530

    setup asto sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,22 +5,23 @@ import remarkParse from 'remark-parse';
 import rehypeStringify from 'rehype-stringify';
 import { remarkReadingTime } from './remark-reading-time.mjs';
 import vercel from '@astrojs/vercel/serverless';
+import sitemap from '@astrojs/sitemap';
 
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), react()],
+  integrations: [tailwind(), mdx(), react(), sitemap()],
   site: 'https://dhrjarun.com',
   base: '/',
   markdown: {
     remarkPlugins: [remarkReadingTime],
-    rehypePlugins: [remarkParse, rehypeStringify]
+    rehypePlugins: [remarkParse, rehypeStringify],
   },
   output: 'server',
   adapter: vercel({
     webAnalytics: {
-      enabled: true
-    }
-  })
+      enabled: true,
+    },
+  }),
 });
